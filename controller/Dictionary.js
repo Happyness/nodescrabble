5af1712cabfce921b657c638e203003d69f33d25
Mats Maatson <mats.maatson@gmail.com>
2013-12-27 12:03:54 +0100

    fixed trim() when reading words from file

--- controller/Dictionary.js
+++ controller/Dictionary.js
@@ -6,7 +6,7 @@ var Dictionary = function(l)
 {
     var fs = require('fs');
     var layout = l;
-    var words;
+    var words = [];
     var letterScores;
 
     var createDictionary = function(layout)
@@ -14,9 +14,9 @@ var Dictionary = function(l)
         switch (layout) {
             case 'default' :
             default :
-                words = fs.readFileSync('./model/sv.txt').toString().split("\n");
-                for(i in words) {
-                    //console.log(words[i]);
+                var data  = fs.readFileSync('./model/sv.txt').toString().split("\n");
+                for(i in data) {
+                    words.push(data[i].trim());
                 }
                 //words = ["is", "jag", "test", "lol", "sa", "dö", "ja", "mig", "ta", "le", "te"];
                 break;
@@ -26,11 +26,13 @@ var Dictionary = function(l)
     var valid = function(word)
     {
         for (var i = 0; i < words.length; i++) {
+            console.log('my word: ' +  word.toLowerCase() + " compared: " + words[i]);
             if (word.toLowerCase() == words[i]) {
+                console.log('DEBUG: TRUE');
                 return true;
             }
         }
-
+        console.log('DEBUG: FALSE');
         return false;
     }
 
